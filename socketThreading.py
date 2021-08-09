846e26c4e7a48fd595a5dc0b49c383592cc88a94
Jacob Paul <p.jacobpaul07@gmail.com>
2021-08-09 21:16:28 +0530

    Update socketThreading.py

--- socketThreading.py
+++ socketThreading.py
@@ -28,6 +28,9 @@ class SocketThread(threading.Thread):
             try:
                 
                 data = self.csocket.recv(1024)
+                if not data:
+                    return
+
                 with global_lock:
                     IST = pytz.timezone('Asia/Kolkata') 
                     dateTimeIND = datetime.datetime.now(IST).strftime("%Y-%m-%dT%H:%M:%S.%f")
@@ -36,58 +39,57 @@ class SocketThread(threading.Thread):
                     print('device number : ' , self.deviceCount)
                     print('thread identity:{0}, device number: {1}'.format(str(threading.get_ident()),str(self.deviceCount)))
                     fData = convert_raw_to_information(data)
+
                     if fData["Live/Memory"] == "L":
-                        print("Live Data: ",data)
-                    else:
-                        print(" 'H' data Received")
-                    if not data:
-                        return
+                        print("Live Data: ",data)     
 
-                   
-                    IMEI = fData["IMEI"]
-                    atIMEI = "@"+IMEI
-                    messageType = "00"
-                    sequenceNumber = fData["Sequence No"]
-                    checkSum = "*CS"
-                    packet = atIMEI,messageType,sequenceNumber,checkSum
-                    seperator = ","
-                    joinedPacket = seperator.join(packet)
-                    bytesPacket = bytes(joinedPacket, 'utf-8')
-                    print("Return Packet:",bytesPacket)
+                        IMEI = fData["IMEI"]
+                        atIMEI = "@"+IMEI
+                        messageType = "00"
+                        sequenceNumber = fData["Sequence No"]
+                        checkSum = "*CS"
+                        packet = atIMEI,messageType,sequenceNumber,checkSum
+                        seperator = ","
+                        joinedPacket = seperator.join(packet)
+                        bytesPacket = bytes(joinedPacket, 'utf-8')
+                        print("Return Packet:",bytesPacket)
 
 
-                    if fData["Message Type"] == "02" and fData["Live/Memory"] == "L":
-                        lat = fData["Latitude"]
-                        lon = fData["Longitude"]
-                        if self.count == 0:
-                            if lat == "":
-                                print("No Lat Lon available")
-                            else:
-                                self.gpslist_lat.insert(0,lat)
-                                self.gpslist_lon.insert(0,lon)
-                                coordinates = {'Latitude' : lat, 'Longitude' : lon,'IMEI': IMEI, 'timestamp' : dateTimeIND} 
-                                self.count += 1
+                        if fData["Message Type"] == "02" and fData["Live/Memory"] == "L":
+                            lat = fData["Latitude"]
+                            lon = fData["Longitude"]
+                            if self.count == 0:
+                                if lat == "":
+                                    print("No Lat Lon available")
+                                else:
+                                    self.gpslist_lat.insert(0,lat)
+                                    self.gpslist_lon.insert(0,lon)
+                                    coordinates = {'Latitude' : lat, 'Longitude' : lon,'IMEI': IMEI, 'timestamp' : dateTimeIND} 
+                                    self.count += 1
 
+                                    # cli.put_object(
+                                    #     Body=str(coordinates),
+                                    #     Bucket='ec2-obd2-bucket',
+                                    #     Key='{0}/Data/{0}_lat_lon_initial.txt'.format(IMEI))
+
+                                    gps_one(lat, lon)
+                                    print("initial:",self.gpslist_lat[0],self.gpslist_lon[0])
+                            else:    
+                                coordinates = {'Latitude' : lat, 'Longitude' : lon, 'IMEI':IMEI, 'timestamp' : dateTimeIND }
+                                
                                 # cli.put_object(
                                 #     Body=str(coordinates),
                                 #     Bucket='ec2-obd2-bucket',
-                                #     Key='{0}/Data/{0}_lat_lon_initial.txt'.format(IMEI))
+                                #     Key='{0}/Data/{0}_lat_lon.txt'.format(IMEI))
+                                # gps_main(self.gpslist_lat[0],self.gpslist_lon[0],lat,lon)
 
-                                gps_one(lat, lon)
                                 print("initial:",self.gpslist_lat[0],self.gpslist_lon[0])
-                        else:    
-                            coordinates = {'Latitude' : lat, 'Longitude' : lon, 'IMEI':IMEI, 'timestamp' : dateTimeIND }
-                            
-                            # cli.put_object(
-                            #     Body=str(coordinates),
-                            #     Bucket='ec2-obd2-bucket',
-                            #     Key='{0}/Data/{0}_lat_lon.txt'.format(IMEI))
-                            # gps_main(self.gpslist_lat[0],self.gpslist_lon[0],lat,lon)
-
-                            print("initial:",self.gpslist_lat[0],self.gpslist_lon[0])
-                            print("live: ",lat,lon)
-                    self.csocket.send(bytesPacket)
-                    print ("Client at", self.clientAddress , "Packet Completely Received...")
+                                print("live: ",lat,lon)
+                        self.csocket.send(bytesPacket)
+                        print ("Client at", self.clientAddress , "Packet Completely Received...")
+                    
+                    else:
+                        print(" 'H' data Received: ",data)
 
             except Exception as exception:
                 print ("Error occured with exception:",exception)
